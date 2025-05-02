6ba5d608b73adeedc51efb243ac90ee8e6f39b67
Hugo Cobos <hugocobos97@gmail.com>
2025-05-02 16:16:47 -0600

    fix

--- android/app/src/main/java/com/hco/sirandroid/MainActivity.java
+++ android/app/src/main/java/com/hco/sirandroid/MainActivity.java
@@ -1,31 +1,5 @@
 package com.hco.sirandroid;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
 
-import com.getcapacitor.Plugin;
-import com.getcapacitor.PluginCall;
-import com.getcapacitor.PluginMethod;
-
-import com.symbol.emdk.EMDKManager;
-
-public class MainActivity extends BridgeActivity {
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // Inicializar el EMDKManager
-    EMDKManager emdkManager = EMDKManager.getEMDKManager(this, new EMDKManager.EMDKListener() {
-      @Override
-      public void onOpened(EMDKManager emdkManager) {
-        // Configurar el escáner y otras funcionalidades
-      }
-
-      @Override
-      public void onClosed() {
-        // Liberar recursos
-      }
-    });
-  }
-}
+public class MainActivity extends BridgeActivity {}
